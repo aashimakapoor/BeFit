74caac32b83d82890e876389c179185699357484
Kapoor, Aashima <aashimakapoor26@gmail.com>
2020-09-25 00:54:11 +0530

    css changes

--- src/constants/header.js
+++ src/constants/header.js
@@ -1,6 +1,12 @@
 export const getHeaderNavLabels = (t) => ({
     title: 'BE FIT',
-    quote: 'Strive for Progress, Not for Perfection',
+    quotes: [
+      'Exercise is King, Nutrition is Queen. Put them together and you have got a Kingdom',
+      'Strive for Progress, Not for Perfection',
+      'To eat is a necessity, but to eat intelligently is an art',
+      'Even in this high-tech age, the low-tech plant continues to be the key to nutrition and health'
+    ],
+    quote: 'Exercise is King, Nutrition is Queen. Put them together and you have got a Kingdom.',
     NAV_ITEMS: [
       {
         label: 'Home',
